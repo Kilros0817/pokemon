c122b538252a0ba46cbd3820dbf63aeb163eece0
Kilros0817 <lazarkitfrl@gmail.com>
2026-06-10 19:33:08 -0700

    chore: configure environment variables for supabase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://gaubkhkbxcxfbeocjdpl.supabase.co',
-  supabaseKey: 'sb_publishable_kbmi34XUG_K-CyMAVE24KA_ilaKGTtl',
+  supabaseUrl: import.meta.env['SUPABASE_URL'] || 'https://gaubkhkbxcxfbeocjdpl.supabase.co',
+  supabaseKey: import.meta.env['SUPABASE_KEY'] || 'sb_publishable_kbmi34XUG_K-CyMAVE24KA_ilaKGTtl',
 };
